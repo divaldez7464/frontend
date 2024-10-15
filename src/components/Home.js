a54dcf8a06978a5b7180f9a362d58e4daf5f40bf
Enrique <enrangel@csumb.edu>
2024-10-14 20:53:04 -0700

    Changed and fixed the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,41 +1,41 @@
-import React, { useState, useEffect } from 'react';
-import { googleLogout, useGoogleLogin } from '@react-oauth/google';
-import { Navigate, useNavigate, useNavigation } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
+import '../styles/Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
     const navigate = useNavigate();
     return (
-        <div>
+        <div className="home-container">
+            <div className="background">
             <Navbar />
-            <div className="home-container">
-                <h1 >Home</h1>
-                <h1>Welcome to the wishlist website</h1>
-                <p className="lead">Begin by adding items to a list</p>
-                
-                <h2>Features Items</h2>
-                <div className="row">
-                    <div className="col-md-4">
-                        <h4>Shoes</h4>
-                        <p>Example</p>
-                    </div>
-                    <div className="col-md-4">
-                        <h4>Games</h4>
-                        <p>Example description</p>
-                    </div>
-                    <div className="col-md-4">
-                        <h4>Sweaters</h4>
-                        <p>Example Descritiption.</p>
-                    </div>
+            <h1>Home</h1>
+            <h1 className="home-container">Welcome To Your Wishlist</h1>
+            <p className="lead">Begin by adding items to a list</p>
+            
+            <h2>Featured Items</h2>
+            <div className="row">
+                <div className="col">
+                    <h4>Shoes</h4>
+                    <p>Example</p>
                 </div>
-
-                <div className="mt-4">
-                    <button className="btn btn-primary" onClick={() => navigate('/wishlist')}>
-                        Item
-                    </button>
+                <div className="col">
+                    <h4>Games</h4>
+                    <p>Example description</p>
+                </div>
+                <div className="col">
+                    <h4>Sweaters</h4>
+                    <p>Example Description.</p>
                 </div>
             </div>
+
+            <div className="mt-4">
+                <button className="btn btn-primary" onClick={() => navigate('/wishlist')}>
+                    Item
+                </button>
+            </div>
+            </div> 
         </div>
     );
 }
