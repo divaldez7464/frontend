f0edb80bcaadd75446908458393fe891bcdf2fd1
Enrique <enrangel@csumb.edu>
2024-10-20 10:47:19 -0700

    added page to add items

--- src/components/Home.js
+++ src/components/Home.js
@@ -48,7 +48,7 @@ function Home() {
                 </div>
             </div>
         </div> 
-
+    </div>
     );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Wishlist</Link>
+        <Link className="navbar-brand" to="/home">Wishlist</Link>
         <button 
           className="navbar-toggler" 
           type="button" 
@@ -19,22 +18,19 @@ function Navbar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
+        <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Login</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/home">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/wishlist">Wishlist</Link>
+              <Link className="nav-link" to="/wishlist">My Wishlist</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/profile">Profile</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/item">Item</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Loggout</Link>
             </li>
           </ul>
         </div>
